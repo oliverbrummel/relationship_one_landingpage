ce2bdae4c3c2bdee657c0e01a1ae8cdc0e9bd3e3
oliverbrummel <oliverbrummel1@gmail.com>
2016-09-16 16:13:07 -0500

    javascript funcionality complete, form info prints in bottom table

--- client.js
+++ client.js
@@ -1,6 +1,37 @@
 $(function(){
 
-console.log('hello from client.js');
+  console.log('hello from client.js');
 
+  var user = {};
 
-});
+  $("#submitButton").on("click", function(event){
+    event.preventDefault();
+
+    user.firstName = $("#firstName").val();
+    user.lastName = $("#lastName").val();
+    user.emailAddress = $("#emailAddress").val();
+    user.company = $("#company").val();
+    user.title = $("#title").val();
+    user.country = $("#country").val();
+    user.state = $("#state").val();
+    user.additionalComments = $("#additionalComments").val();
+
+    displayUserInfo(user);
+  });//closes submitButton.on('click')
+
+  var displayUserInfo = function(user){
+    $("#formDisplay").prepend("<h4>Form Results:</h4>")
+
+    $("#formDisplay > tbody").append("<tr><td>First Name</td><td>"+user.firstName+"</td></tr>");
+    $("#formDisplay > tbody").append("<tr><td>Last Name</td><td>"+user.lastName+"</td></tr>");
+    $("#formDisplay > tbody").append("<tr><td>Email Address</td><td>"+user.emailAddress+"</td></tr>");
+    $("#formDisplay > tbody").append("<tr><td>Company</td><td>"+user.company+"</td></tr>");
+    $("#formDisplay > tbody").append("<tr><td>Title</td><td>"+user.title+"</td></tr>");
+    $("#formDisplay > tbody").append("<tr><td>Country</td><td>"+user.country+"</td></tr>");
+    $("#formDisplay > tbody").append("<tr><td>State</td><td>"+user.state+"</td></tr>");
+    $("#formDisplay > tbody").append("<tr><td>Additional Comments</td><td>"+user.additionalComments+"</td></tr>");
+
+    $("td").addClass("col-xs-6");
+  }
+
+});//closes document.ready
